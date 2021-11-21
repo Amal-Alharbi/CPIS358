90bdaa9fa65d921789107f98ae9e8f7aee65fb91
Amal Alharbi <ahalharbi1@sheffield.ac.uk>
2021-11-21 23:14:31 +0300

    Add files via upload

---
+++ Shopping.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookStore.Pages
+{
+    public class ShoppingModel : PageModel
+    {
+
+        public string WelcomeUsername { get; set; }
+
+        private BookStore.Data.BookStoreContext context;
+        public List<Book> allBooks;
+
+        public ShoppingModel(BookStore.Data.BookStoreContext _context)
+        {
+            context = _context;
+        }
+
+
+
+        public void OnGet()
+        {
+            WelcomeUsername = HttpContext.Session.GetString("username");
+
+            allBooks = context.Book.ToList();
+
+        }
+
+        public IActionResult OnGetLogout()
+        {
+
+            HttpContext.Session.Remove("username");
+
+            return RedirectToPage("Index");
+        }
+
+    }
+}
