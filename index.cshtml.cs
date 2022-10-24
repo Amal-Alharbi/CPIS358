68ae8c46eb63abac685a5bd8a53e8d432e976413
Amal Alharbi <ahalharbi1@sheffield.ac.uk>
2022-10-24 10:06:57 +0300

    Create index.cshtml.cs

---
+++ index.cshtml.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace BookStore.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly BookStore.Data.UserContext _context;
+
+        public IndexModel(BookStore.Data.UserContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public string formUsername { get; set; }
+        [BindProperty]
+        public string formPassword { get; set; }
+        public string Msg { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            var tempUser = _context.User.FirstOrDefault(u => u.username == formUsername && u.password == formPassword);
+            if (tempUser != null)
+            {
+                HttpContext.Session.SetString("username", formUsername);
+                return RedirectToPage("Shopping");
+            }
+            else
+            {
+                Msg = "Inavalid username or password.";
+                return Page();
+            }
+        }
+    }
+}
