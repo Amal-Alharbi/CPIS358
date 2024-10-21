5a8013e8c0016c1180959c39cba60024a45dcbb8
Amal Alharbi <ahalharbi1@sheffield.ac.uk>
2024-10-21 13:25:30 +0300

    Update Book.cs

--- Book.cs
+++ Book.cs
@@ -1,11 +1,9 @@
-using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace BookStore.Models
+namespace BookStore.Data
 {
     public class Book
     {
-
         public int ID { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
@@ -14,6 +12,5 @@ namespace BookStore.Models
         public double Pages { get; set; }
         public double Price { get; set; }
         public string photo { get; set; }
-
     }
 }
